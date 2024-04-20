3b7093a632dd4db77c1c044780240a5d12c077af
Karol <kpulawski13@gmail.com>
2024-04-20 16:50:31 +0100

    Add connection to the DB

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Connect to the database
+require('./config/db');
+
+// Gets routers
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
