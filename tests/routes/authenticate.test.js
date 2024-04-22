8e82a3854fdd60750cd7900d3b5dcdf3eb7ff6da
Karol <kpulawski13@gmail.com>
2024-04-22 19:08:40 +0100

    Remove try catch from tests

--- tests/routes/authenticate.test.js
+++ tests/routes/authenticate.test.js
@@ -27,47 +27,32 @@ jest.mock('../../models/user', () => {
 
 describe('POST /sign-up', () => {
   test('should register a new user', async () => {
-    try {
-      const response = await request(app).post('/sign-up').send({
-        username: 'newuser',
-        password: 'password123',
-      });
-      expect(response.status).toBe(201);
-      expect(response.body.user).toHaveProperty('username', 'newuser');
-      expect(response.body).toHaveProperty('token');
-    } catch (error) {
-      throw new Error(
-        'Failed to test sign-up due to unexpected error:',
-        +error
-      );
-    }
+    const response = await request(app).post('/sign-up').send({
+      username: 'newuser',
+      password: 'password123',
+    });
+    expect(response.status).toBe(201);
+    expect(response.body.user).toHaveProperty('username', 'newuser');
+    expect(response.body).toHaveProperty('token');
   });
 });
 
 describe('POST /login', () => {
   test('Should authenticate user and return token', async () => {
-    try {
-      const response = await request(app).post('/login').send({
-        username: 'testuser',
-        password: 'password123',
-      });
+    const response = await request(app).post('/login').send({
+      username: 'testuser',
+      password: 'password123',
+    });
 
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('token');
-    } catch (error) {
-      throw new Error('Failed to test login due to unexpected error', +error);
-    }
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
   });
 
   test('should reject invalid login try', async () => {
-    try {
-      const response = await request(app)
-        .post('/login')
-        .send({ username: 'userNotFound', password: 'wrongPassword' });
+    const response = await request(app)
+      .post('/login')
+      .send({ username: 'userNotFound', password: 'wrongPassword' });
 
-      expect(401);
-    } catch (error) {
-      throw new Error('Failed to test login due to unexpected error', error);
-    }
+    expect(401);
   });
 });
