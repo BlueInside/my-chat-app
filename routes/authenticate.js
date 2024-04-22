0d4755c77b2bd609141f02b1f4122ee14a1f258f
Karol <kpulawski13@gmail.com>
2024-04-22 09:40:52 +0100

    Require and add validations to authenticate route handler

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const authenticate = express.Router();
-const { sign_up } = require('../controllers/authenticateController');
+const {
+  validateSignup,
+  validateLogin,
+  validate,
+} = require('../lib/validations');
+const { sign_up, login } = require('../controllers/authenticateController');
 
 // POST sign-up
-authenticate.post('/sign-up', sign_up);
+authenticate.post('/sign-up', validateSignup, validate, sign_up);
 
+// POST login
+authenticate.post('/login', validateLogin, validate, login);
 module.exports = authenticate;
