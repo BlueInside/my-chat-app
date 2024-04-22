81eacaf6f1d3a14c4c8e21b82a0caf6c353c35fc
Karol <kpulawski13@gmail.com>
2024-04-22 08:40:51 +0100

    Test /login route

--- tests/routes/authenticate.test.js
+++ tests/routes/authenticate.test.js
@@ -8,29 +8,66 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', authenticate);
 
-jest.mock('../../models/user', () => ({
-  findOne: jest.fn().mockResolvedValue({ id: 1, username: 'testuser' }),
-  create: jest
-    .fn()
-    .mockResolvedValue({ id: 1, username: 'newuser', token: 'fakeToken' }),
-}));
+jest.mock('../../models/user', () => {
+  // Require bcrypt and hash password
+  const bcrypt = require('bcryptjs');
+  const mockHashedPassword = bcrypt.hashSync('password123', 10);
+
+  return {
+    findOne: jest.fn().mockResolvedValue({
+      id: 1,
+      username: 'testuser',
+      password: mockHashedPassword,
+    }),
+    create: jest
+      .fn()
+      .mockResolvedValue({ id: 1, username: 'newuser', token: 'fakeToken' }),
+  };
+});
 
 describe('POST /sign-up', () => {
-  test('should register a new user', (done) => {
-    request(app)
-      .post('/sign-up')
-      .send({
+  test('should register a new user', async () => {
+    try {
+      const response = await request(app).post('/sign-up').send({
         username: 'newuser',
         password: 'password123',
-      })
-      .then((response) => {
-        expect(response.status).toBe(201);
-        expect(response.body.user).toHaveProperty('username', 'newuser');
-        expect(response.body).toHaveProperty('token');
-        done();
-      })
-      .catch((error) => {
-        done(error);
       });
+      expect(response.status).toBe(201);
+      expect(response.body.user).toHaveProperty('username', 'newuser');
+      expect(response.body).toHaveProperty('token');
+    } catch (error) {
+      throw new Error(
+        'Failed to test sign-up due to unexpected error:',
+        +error
+      );
+    }
+  });
+});
+
+describe('POST /login', () => {
+  test('Should authenticate user and return token', async () => {
+    try {
+      const response = await request(app).post('/login').send({
+        username: 'testuser',
+        password: 'password123',
+      });
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('token');
+    } catch (error) {
+      throw new Error('Failed to test login due to unexpected error', +error);
+    }
+  });
+
+  test('should reject invalid login try', async () => {
+    try {
+      const response = await request(app)
+        .post('/login')
+        .send({ username: 'userNotFound', password: 'wrongPassword' });
+
+      expect(401);
+    } catch (error) {
+      throw new Error('Failed to test login due to unexpected error', error);
+    }
   });
 });
