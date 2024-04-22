f678eecc364318ea47913067f9f279b4522a2cf0
Karol <kpulawski13@gmail.com>
2024-04-22 19:09:16 +0100

    Create router for /users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const {
+  createUserValidationRules,
+  updateUserValidationRules,
+  validate,
+} = require('../lib/userValidations');
+const userController = require('../controllers/usersController');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+// GET all users
+router.get('/', userController.getAllUsers);
+
+// GET single user
+router.get('/:id', userController.getUserById);
+
+// POST new user
+router.post(
+  '/',
+  createUserValidationRules(),
+  validate,
+  userController.createUser
+);
+
+// PUT to edit existing user
+router.put(
+  '/:id',
+  updateUserValidationRules(),
+  validate,
+  userController.updateUser
+);
+
+// DELETE to remove existing user
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
