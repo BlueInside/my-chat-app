5fac5bfebe3bf96f4cf017e8cc426658e8ea2f05
Karol <kpulawski13@gmail.com>
2024-04-22 19:43:13 +0100

    Create messages router

---
+++ routes/messages.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const messages = express.Router();
+
+const messageController = require('../controllers/messageController');
+// GET all messages for a user
+messages.get('/', messageController.getAllMessages);
+
+// GET specific message by ID
+messages.get('/:id', messageController.getMessageById);
+
+// Send new message with POST
+messages.post('/', messageController.sendMessage);
+
+// Delete a message
+messages.delete('/:id', messageController.deleteMessage);
+
+module.exports = messages;
