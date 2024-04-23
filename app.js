3c47f9fba3335faae9962a9d47d9d17eedcda27f
Karol <kpulawski13@gmail.com>
2024-04-23 08:28:35 +0100

    Add messagesRouter to the app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('./config/db');
 // Gets routers
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const messagesRouter = require('./routes/messages');
 const app = express();
 
 app.use(logger('dev'));
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/messages', messagesRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
