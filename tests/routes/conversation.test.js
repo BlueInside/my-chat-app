59a8c55f653e1ddb535c5a1603f781f9a3ca3f27
Karol <kpulawski13@gmail.com>
2024-04-25 12:43:33 +0100

    Write test to get all user specific conversation and write test for POST

--- tests/routes/conversation.test.js
+++ tests/routes/conversation.test.js
@@ -1,6 +1,12 @@
 const conversationRouter = require('../../routes/conversation');
 const express = require('express');
 const request = require('supertest');
+const ObjectId = require('mongoose').Types.ObjectId;
+// mock Conversation model;
+
+jest.mock('../../models/conversation', () => ({
+  find: jest.fn().mockResolvedValue([{ id: 1 }, { id: 2 }]),
+}));
 
 const app = express();
 
@@ -8,3 +14,27 @@ app.use('/conversations', conversationRouter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+describe('GET /conversations', () => {
+  test('Should get all user specific conversations', async () => {
+    const response = await request(app).get('/conversations');
+
+    expect(response.status).toBe(200);
+    expect(response.body.conversations).toHaveLength(2);
+  });
+});
+
+describe('POST /conversations', () => {
+  test('Should create new conversation between users', async () => {
+    let senderId = new ObjectId();
+    let receiverId = new ObjectId();
+
+    const response = await request(app)
+      .post('/conversations')
+      .send({ participants: [senderId, receiverId] });
+
+    expect(response.status).toBe(201);
+    expect(response.body.conversation).toBeInstanceOf(Object);
+    expect(response.body.messages).toBeInstanceOf(Array);
+  });
+});
