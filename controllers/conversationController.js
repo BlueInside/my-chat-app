11696663c2455d41223cb5c82909ca4860a9415f
Karol <kpulawski13@gmail.com>
2024-04-25 12:44:06 +0100

    Add implementation to getAllConversations without using jwt

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -2,12 +2,14 @@ const Conversation = require('../models/conversation');
 const asyncHandler = require('express-async-handler');
 
 const getAllConversations = asyncHandler(async (req, res) => {
-  res.send('NOT IMPLEMENTED ');
+  // Use jwt payload to get userId
+  const userId = 2;
+  const conversations = await Conversation.find({ participants: userId });
+
+  res.status(200).json({ conversations: conversations });
 });
 
-const createConversation = asyncHandler(async (req, res) => {
-  res.send('NOT IMPLEMENTED ');
-});
+const createConversation = asyncHandler(async (req, res) => {});
 
 const getConversationDetails = asyncHandler(async (req, res) => {
   const { id } = req.params;
