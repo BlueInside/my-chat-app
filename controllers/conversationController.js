b30c45c43f36dc3bfae91be47edcc0cc535ea8d1
Karol <kpulawski13@gmail.com>
2024-04-25 13:44:05 +0100

    Implement function to create new conversation

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -1,6 +1,6 @@
 const Conversation = require('../models/conversation');
 const asyncHandler = require('express-async-handler');
-
+const isValid = require('mongoose').Types.ObjectId.isValid;
 const getAllConversations = asyncHandler(async (req, res) => {
   // Use jwt payload to get userId
   const userId = 2;
@@ -9,7 +9,44 @@ const getAllConversations = asyncHandler(async (req, res) => {
   res.status(200).json({ conversations: conversations });
 });
 
-const createConversation = asyncHandler(async (req, res) => {});
+const createConversation = asyncHandler(async (req, res) => {
+  // USE JWT
+
+  const { senderId, receiverId } = req.body;
+
+  //   Check if id's are correct mongoose ids
+  if (!isValid(senderId) || !isValid(receiverId)) {
+    return res
+      .status(400)
+      .json({ message: 'Either sender or receiver id is wrong.' });
+  }
+
+  if (senderId === receiverId) {
+    return res
+      .status(400)
+      .json({ message: 'Sender and receiver cannot be the same.' });
+  }
+
+  try {
+    const sortedParticipants = [senderId, receiverId].sort(); // Sort participants
+    const conversation = await Conversation.create({
+      participants: sortedParticipants,
+    });
+
+    if (!conversation) {
+      return next(new Error('Failed creating a new conversation'));
+    }
+
+    return res.status(201).json({ conversation: conversation });
+  } catch (error) {
+    if (error.code === 11000) {
+      // MongoDB duplicate key error
+      res.status(409).json({ message: 'Conversation already exists' });
+    } else {
+      return next(error);
+    }
+  }
+});
 
 const getConversationDetails = asyncHandler(async (req, res) => {
   const { id } = req.params;
