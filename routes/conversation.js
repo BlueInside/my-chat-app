273d5b681fd834138d94f8c96c59e1bb07948876
Karol <kpulawski13@gmail.com>
2024-04-25 18:32:17 +0100

    Call conversationValidation rules on post route

--- routes/conversation.js
+++ routes/conversation.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const conversation = express.Router();
 const conversationController = require('../controllers/conversationController.js');
+const {
+  createConversationValidation,
+} = require('../lib/conversationValidations.js');
 
 // Gets all user conversations
 conversation.get('/', conversationController.getAllConversations);
 
 // Create new conversation
-conversation.post('/', conversationController.createConversation);
-
+conversation.post(
+  '/',
+  createConversationValidation(),
+  conversationController.createConversation
+);
 // Get details of specific conversation
 conversation.get('/:id', conversationController.getConversationDetails);
 
