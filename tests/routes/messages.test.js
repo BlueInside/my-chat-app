897a1b3c1e3e358b4a70339bd79720e3fcba66d0
Karol <kpulawski13@gmail.com>
2024-04-25 09:22:30 +0100

    Write mock for Conversation, Message models and implement test for POST /messages

--- tests/routes/messages.test.js
+++ tests/routes/messages.test.js
@@ -9,9 +9,28 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/messages', messages);
 
+// Mock conversation model
+jest.mock('../../models/conversation', () => ({
+  findOne: jest.fn().mockResolvedValue({
+    id: '1',
+    messages: [],
+    lastMessage: '',
+    save: jest.fn().mockResolvedValue({}),
+  }),
+}));
+
+// Mock messages model
 jest.mock('../../models/message', () => ({
   find: jest.fn().mockResolvedValue([]),
   findById: jest.fn().mockResolvedValue({ id: '1', text: 'Hello World' }),
+  create: jest.fn().mockImplementation((data) =>
+    Promise.resolve({
+      id: data.sender,
+      ...data,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    })
+  ),
 }));
 
 describe('GET /messages', () => {
@@ -29,5 +48,22 @@ describe('GET /messages', () => {
     expect(response.status).toBe(200);
     expect(response.body.message).toBeInstanceOf(Object);
     expect(response.body.message.text).toBe('Hello World');
+  });
+});
+
+describe('POST /messages', () => {
+  test('Successfully sends message', async () => {
+    const data = {
+      receiverId: new ObjectId().toString(),
+      senderId: new ObjectId().toString(),
+      text: 'Hello world',
+    };
+
+    const response = await request(app).post('/messages').send(data);
+
+    expect(response.status).toBe(201);
+    expect(response.body.data.text).toBe(data.text);
+    expect(response.body.data.receiver).toBe(data.receiverId);
+    expect(response.body.data.sender).toBe(data.senderId);
   });
 });
