fe0edd66f14f9acd3a35a045cad082ed684abb86
Karol <kpulawski13@gmail.com>
2024-04-25 11:09:57 +0100

    Create conversation route handler

---
+++ routes/conversation.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const conversation = express.Router();
+const conversationController = require('../controllers/conversationController.js');
+
+// Gets all user conversations
+conversation.get('/', conversationController.getAllConversations);
+
+// Create new conversation
+conversation.post('/', conversationController.createConversation);
+
+// Get details of specific conversation
+conversation.get('/:id', conversationController.getConversationDetails);
+conversation;
