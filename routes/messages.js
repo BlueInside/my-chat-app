41cba37e87abf2f7760b10179ec4333bab4cc36c
Karol <kpulawski13@gmail.com>
2024-04-26 09:09:40 +0100

    Add validations to /:id in messages route

--- routes/messages.js
+++ routes/messages.js
@@ -2,12 +2,16 @@ const express = require('express');
 const messages = express.Router();
 
 const messageController = require('../controllers/messageController');
-
+const { getMessageByIdValidation } = require('../lib/messagesValidation');
 // GET all messages for a user
 messages.get('/', messageController.getAllMessages);
 
 // GET specific message by ID
-messages.get('/:id', messageController.getMessageById);
+messages.get(
+  '/:id',
+  getMessageByIdValidation(),
+  messageController.getMessageById
+);
 
 // Send new message with POST
 messages.post('/', messageController.sendMessage);
