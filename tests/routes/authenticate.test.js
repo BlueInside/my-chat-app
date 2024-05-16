dee648fd5290bca7a3ed6f6a4a1034410e5c5bd3
Karol <kpulawski13@gmail.com>
2024-05-16 18:01:28 +0100

    Test post/verify-token route

--- tests/routes/authenticate.test.js
+++ tests/routes/authenticate.test.js
@@ -1,12 +1,21 @@
 const authenticate = require('../../routes/authenticate');
 const request = require('supertest');
 const express = require('express');
+const { generateToken } = require('../../lib/jwt');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', authenticate);
+
+let mockId = new ObjectId().toString();
+const mockToken = generateToken({
+  id: mockId,
+  username: 'karol',
+  role: 'user',
+});
 
 jest.mock('../../models/user', () => {
   // Require bcrypt and hash password
@@ -25,9 +34,9 @@ jest.mock('../../models/user', () => {
   };
 });
 
-describe('POST /sign-up', () => {
+describe('POST /register', () => {
   test('should register a new user', async () => {
-    const response = await request(app).post('/sign-up').send({
+    const response = await request(app).post('/register').send({
       username: 'newuser',
       password: 'password123',
     });
@@ -54,5 +63,20 @@ describe('POST /login', () => {
       .send({ username: 'userNotFound', password: 'wrongPassword' });
 
     expect(401);
+  });
+});
+
+describe('Post /verify-token', () => {
+  test('Should verify token and return user', async () => {
+    const response = await request(app)
+      .get('/verify-token')
+      .set('authorization', `Bearer ${mockToken}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.user.id).toMatch(mockId);
+    expect(response.body.user).toHaveProperty('username');
+    expect(response.body.user.username).toMatch(/karol/i);
+    expect(response.body.user).toHaveProperty('role');
+    expect(response.body.user.role).toMatch(/user/i);
   });
 });
