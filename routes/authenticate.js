e875d92807e0a89da0c78d9b06d990a39490c3f1
Karol <kpulawski13@gmail.com>
2024-05-16 18:00:50 +0100

    Rename sign-up to register

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const authenticate = express.Router();
+const { loginValidation, registerValidation } = require('../lib/validations');
 const {
-  validateSignup,
-  validateLogin,
-  validate,
-} = require('../lib/validations');
-const { signUp, login } = require('../controllers/authenticateController');
-
-// POST sign-up
-authenticate.post('/sign-up', validateSignup, validate, signUp);
+  signUp,
+  login,
+  verifyToken,
+} = require('../controllers/authenticateController');
+const { authenticateToken } = require('../lib/jwt.js');
+// POST register
+authenticate.post('/register', registerValidation(), signUp);
 
 // POST login
-authenticate.post('/login', validateLogin, validate, login);
+authenticate.post('/login', loginValidation(), login);
+
+// Post verify-token
+authenticate.get('/verify-token', authenticateToken, verifyToken);
+
 module.exports = authenticate;
